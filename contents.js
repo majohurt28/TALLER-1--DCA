72a7c895b3ed0f002d06d2b9d49a3cd3b335d9bd
majohurt28 <maria.hurtado28@hotmail.com>
2023-02-17 19:02:14 -0500

    card content

---
+++ contents.js
@@ -0,0 +1,15 @@
+const videoCard =[{
+
+    img: 'https://i.ytimg.com/an_webp/7NK_JOkuSVY/mqdefault_6s.webp?du=3000&sqp=CK75v58G&rs=AOn4CLBBR-IoWAJ4th08_bctV0taTA_H1w',
+    name: "Numb [Official Music Video] - Linkin Park",
+    profile: "Linkin Park",
+    description: "16M de vistas . hace 7 días",
+},
+{
+    img: 'https://i.ytimg.com/an_webp/xKHUwfFb2lw/mqdefault_6s.webp?du=3000&sqp=CPigwJ8G&rs=AOn4CLBBdzi84B5FF1wHYQBJVX2AWTolng',
+    name: "Alice In Chains - Get Born Again (Official Video)",
+    profile: "Alice In Chains",
+    description: "5.6M de vistas . hace 13 años",
+},
+]
+
---
+++ index.js
@@ -0,0 +1,4 @@
+const barContainer = document.getElementById("bar");
+const optionsContainer = document.getElementById("options");
+const videosContainer = document.getElementById("videos");
+const cateContainer = document.getElementById("categories");
