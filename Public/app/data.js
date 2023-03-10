4fa22a25f2afa2fc6b23766c6cca43d913fcab32
majohurt28 <113456623+majohurt28@users.noreply.github.com>
2023-03-09 23:15:40 -0500

    Data complete

--- Public/app/data.js
+++ Public/app/data.js
@@ -11,5 +11,49 @@ const data =[{
     profile: "Alice In Chains",
     description: "5.6M de vistas . hace 13 años",
 },
-]
+{
+    img: 'https://i.ytimg.com/vi/sVx1mJDeUjY/hq720.jpg?sqp=-oaymwE2COgCEMoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARh_IBMoGjAP&rs=AOn4CLD6TaLzjAkxFVe7C1ubWLlPD92keg',
+    name: "Mr.Kitty - After Dark",
+    profile: "Mr.Kitty Official",
+    description: "108M de vistas . hace 8 años",
+
+},
+{
+    img: 'https://i.ytimg.com/vi/Qt298P1kL8E/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBeGcXWFwIk54qlKEa2BNj-Cwlb9Q',
+    name: "Evanescence - Whisper",
+    profile: "Evanescence",
+    description: "3.8M de vistas . hace 6 años",
+},
+{
+    img: 'https://i.ytimg.com/vi/zM1Y75jNaZ0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAivG8yU-OpbbCBjMGYKmmABbYvpQ',
+    name: "Judas Priest - Angel (Audio)",
+    profile: "Judas Priest",
+    description: "3.4M de vistas . hace 5 años",
+
+},
+{
+    img: 'https://i.ytimg.com/an_webp/nTJHkUze9lc/mqdefault_6s.webp?du=3000&sqp=CJmWqqAG&rs=AOn4CLA8O366MWsR4IrW8nt1jiyLWp5WhQ',
+    name: "Pantera - Domination (2010 remaster)",
+    profile: "Pantera",
+    description: "7.3M de vistas . hace 8 años",
+
+},
+{
+    img: 'https://i.ytimg.com/an_webp/oSHzUD-uqKY/mqdefault_6s.webp?du=3000&sqp=CNi-qqAG&rs=AOn4CLDTjRSQsbMbv8tA49F0xfAeYP4wyw',
+    name: "Elley Duhé - MIDDLE OF THE NIGHT (Official Video)",
+    profile: "Elley Duhé",
+    description: "69M de vistas . hace 10 meses",
+
+},
+{
+    img: 'https://i.ytimg.com/vi/-gZC9hC1PDQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCbLZ-KM51iFv8CRKutABm0xxdvkA',
+    name: "Kick it - BLACKPINK",
+    profile: "BLACKPINK",
+    description: "26M de vistas . hace 2 años",
+
+},
+
+];
+
+export default data;
 
