0507a6ff37369f2f139d11f5eb0bce4bde4dbf3b
ZoulA2 <112178031+ZoulA2@users.noreply.github.com>
2023-03-10 02:41:27 -0500

    minor changes
    minor changes

--- Public/app/components/index.js
+++ Public/app/components/index.js
@@ -1,1 +1,1 @@
-export{default as cardContainer} from './videoCards/cards.js'
+export{default as Card} from './videoCards/cards.js'
--- Public/app/index.js
+++ Public/app/index.js
@@ -1,21 +1,21 @@
-import * as videoCards from "./components/videoCards/cards.js"
+import * as videoCards from "./components/index.js"
 import data from "./data.js"
 class VideoContainer extends HTMLElement {
 Constructor(){
-Super()
+Super();
 This.attachShadow({mode: 'open'});
 }
 connectedCallback(){
 this.render();
 }
 render(){
-data.forEach((card)=>{
-this.shadowRoot. innerHTML += `
-<video="${card.img}" name="${card.name}" profile="${card.profile}" views="${card.description.views} date="${card.description.date}>
-</video>
+data.forEach((card)=> {
+this.shadowRoot.innerHTML += `
+<My-card video="${card.img}" name="${card.name}" profile="${card.profile}" views="${card.description.views} date="${card.description.date}>
+</My-card>
 `
 })
 }
 }
-customElements.define("app-container",VideoContainer);
+customElements.define("Video-container",VideoContainer);
 
